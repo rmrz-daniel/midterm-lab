235c90c8c2f1eb23b09c035b0c6da29b39471803
Daniel Ramirez <ramirez.daniel1204@gmail.com>
2022-06-30 22:21:13 -0600

    Adds cards

--- src/Components/Students.js
+++ src/Components/Students.js
@@ -1,12 +1,79 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from "react-router-dom"
+import { Container, Card, Row, Col, Image, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export class Students extends Component {
-  render() {
+
+
+function Students({students}) {
     return (
-      <div><Link to="/test">Test</Link></div>
+        <div>
+            <Card className='mb-3 text-center' style={{ width: '12rem' }}>
+                <Card.Header as="h5">Student</Card.Header>
+                <Image 
+                src={require(`./images/${students.image}`).default}
+                className="card-img-top" 
+                alt="student Profile Pic" 
+                fluid
+                />
+                <Card.Body>
+                    <Card.Title>
+                        {students.name}
+                    </Card.Title>
+                    <Card.Text>
+                        <p>
+                            GPA: <strong>{students.gpa}</strong>
+                        </p>
+                    </Card.Text>
+                    <Button variant="primary">Grade</Button>
+                </Card.Body>
+            </Card>
+        </div>
     )
-  }
 }
 
-export default Students
+
+
+function StudentsList() {
+    const cardInfo = [
+        {
+            image: "s1.png",
+            name: "Mike",
+            gpa: 3.8
+        },
+        {
+            image: "s2.png",
+            name: "Manal",
+            gpa: 3.6
+        },
+        {
+            image: "s3.png",
+            name: "Moe",
+            gpa: 3.2
+        },
+        {
+            image: "s4.png",
+            name: "Mary",
+            gpa: 2.6
+        }
+    ]
+
+    return (
+        <div className='text-center'>
+            <h2>Student List:</h2>
+            <Link to="/test" >Go to test page</Link>
+            <Container>
+                <Row>
+                    <Col md={3}>
+                        {cardInfo.map(students =>
+                        <Students students = {students}/>
+                        )}
+                    </Col>
+                </Row>
+            </Container>
+            
+        </div>
+    )
+}
+
+export default StudentsList
--- src/Components/testpage.js
+++ src/Components/testpage.js
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom"
 export default class TestPage extends Component {
   render() {
     return (
-      <div><Link to="/">Student</Link></div>
+      <div>
+        <p>This is a test page to show case functionality of page routing</p>
+        <Link to="/">go back to Student list</Link>
+        </div>
     )
   }
 }
