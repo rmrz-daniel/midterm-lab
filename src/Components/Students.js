5eeb96ce0fc7ee2b8c678143f773111c6ef73a17
Daniel Ramirez <ramirez.daniel1204@gmail.com>
2022-06-30 19:00:59 -0600

    Creates class components in new files

---
+++ src/Components/Students.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Students extends Component {
+  render() {
+    return (
+      <div>Students</div>
+    )
+  }
+}
+
+export default Students
---
+++ src/Components/testpage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class testpage extends Component {
+  render() {
+    return (
+      <div>testpage</div>
+    )
+  }
+}
